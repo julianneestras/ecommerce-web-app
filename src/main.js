f3386243f0b74d6826018171049facd430c055c1
julianneestras <julianneestras@gmail.com>
2025-04-24 17:51:21 +0800

    ✨ feat: Import SvgIcon component and svg-icons-register for dynamic icons

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router/main.js";
 import { createPinia } from "pinia";
+import SvgIcon from "./components/SvgIcon.vue";
+import "virtual:svg-icons-register";
 
 const app = createApp(App);
 const pinia = createPinia();
 
+app.component("SvgIcon", SvgIcon);
 app.use(pinia);
 app.use(router);
 app.mount("#app");
