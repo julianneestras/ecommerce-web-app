d30ab330fbaaf391e7c748d186b957f390ab5c3c
julianneestras <julianneestras@gmail.com>
2025-05-05 15:52:43 +0800

    ✨ feat: Add a Products view page

---
+++ src/views/Products.vue
@@ -0,0 +1,19 @@
+<template>
+    <ProductCard :products="productStore.products">
+        <template v-slot:title>
+            <h1>Explore All Items</h1>
+        </template>
+    </ProductCard>
+</template>
+
+<script setup lang="js">
+import { onMounted } from 'vue';
+import { useProductStore } from '../store/product.js';
+import ProductCard from '../components/ProductCard.vue';
+
+const productStore = useProductStore();
+
+onMounted(() => {
+    productStore.fetchAllProducts()
+})
+</script>
