5694ad1e12ef0acee244836d869d599b7fb79912
julianneestras <julianneestras@gmail.com>
2025-05-07 18:06:10 +0800

    ✨ feat: Rename to CartIcon.vue

---
+++ src/components/CartIcon.vue
@@ -0,0 +1,16 @@
+<template>
+    <button @click="$router.push('/cart')" class="relative inline-block w-5.5 h-5.5 cursor-pointer">
+        <svg-icon :name="'bag'" class="w-full h-full text-gray-900 dark:text-gray-300" />
+        <div
+            class="absolute -bottom-1 -right-1 w-4 h-4 bg-red-500 text-white text-[10px] font-semibold rounded-full flex items-center justify-center shadow-md">
+            {{ cartCount }}
+        </div>
+    </button>
+</template>
+
+<script setup lang="js">
+import { ref } from 'vue'
+
+const cartCount = ref(0)
+
+</script>
