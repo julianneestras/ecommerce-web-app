1e08e96fcef09dbbba381fd859d6d49bee6f6e4c
julianneestras <julianneestras@gmail.com>
2025-05-14 10:37:23 +0800

    ✨ feat: Connect cart count to store and hide badge when zero

--- src/components/CartIcon.vue
+++ src/components/CartIcon.vue
@@ -1,7 +1,7 @@
 <template>
     <button @click="$router.push('/cart')" class="relative inline-block w-5.5 h-5.5 cursor-pointer">
         <svg-icon :name="'bag'" class="w-full h-full text-gray-900 dark:text-gray-300" />
-        <div
+        <div v-if="cartCount > 0"
             class="absolute -bottom-1 -right-1 w-4 h-4 bg-red-500 text-white text-[10px] font-semibold rounded-full flex items-center justify-center shadow-md">
             {{ cartCount }}
         </div>
@@ -9,8 +9,9 @@
 </template>
 
 <script setup lang="js">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import { useCartStore } from '../store/cart.js';
 
-const cartCount = ref(0)
-
+const cartStore = useCartStore()
+const cartCount = computed(() => cartStore.cartCount)
 </script>
